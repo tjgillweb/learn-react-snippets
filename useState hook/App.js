502c887a02e48b93e89e557b5f162f23fbc95015
tjgillweb <tjgillweb@gmail.com>
2020-06-20 17:30:37 +0530

    useState with array

--- useState hook/App.js
+++ useState hook/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import ClassCounter from './components/ClassCounter';
-import HookCounterObject from './components/HookCounterObject';
+import HookCounterArray from './components/HookCounterArray';
 
 function App() {
   return (
     <div className="App">
-      <HookCounterObject />
+      <HookCounterArray />
     </div>
   );
 }
