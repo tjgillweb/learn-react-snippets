b7021bbb7d4ff74262e9db319c7892bcef6d74e2
tjgillweb <tjgillweb@gmail.com>
2020-06-20 23:45:08 +0530

    Add useEffect after render

---
+++ useEffect hook/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import ClassCounterOne from './components/ClassCounterOne';
+import HookCounterOne from './components/HookCounterOne';
+
+function App() {
+  return (
+    <div className="App">
+      <ClassCounterOne />
+      <HookCounterOne />
+    </div>
+  );
+}
+
+export default App;
---
+++ useEffect hook/components/HookCounterOne.js
@@ -0,0 +1,18 @@
+import React, {useState, useEffect} from 'react';
+
+const HookCounterOne = () => {
+    const [count, setCount] = useState(0)
+
+    useEffect(() => {
+        document.title = `You clicked ${count} times`
+    })
+    return ( 
+        <div>
+            <button onClick={() => setCount(count + 1)}>
+                Click {count} times
+            </button>
+        </div>
+     );
+}
+ 
+export default HookCounterOne;
