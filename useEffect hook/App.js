690dc27f2220579ccf874ad388cc9f721dcb4d0b
tjgillweb <tjgillweb@gmail.com>
2020-06-21 21:25:41 +0530

    Run effects only once

--- useEffect hook/App.js
+++ useEffect hook/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import ClassCounterOne from './components/ClassCounterOne';
-import HookCounterOne from './components/HookCounterOne';
+import ClassMouse from './components/ClassMouse';
+import HookMouse from './components/HookMouse';
 
 function App() {
   return (
     <div className="App">
-      {/* <ClassCounterOne /> */}
-      <HookCounterOne />
+      <ClassMouse />
+      <HookMouse />
     </div>
   );
 }
