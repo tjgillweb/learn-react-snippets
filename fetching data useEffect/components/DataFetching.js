4bd547d5e7d6fe40a173880c98f136cd3396916f
tjgillweb <tjgillweb@gmail.com>
2020-06-24 21:38:12 +0530

    fetch data on button click

--- fetching data useEffect/components/DataFetching.js
+++ fetching data useEffect/components/DataFetching.js
@@ -2,32 +2,30 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios'
 
 const DataFetching = () => {
-    // const [posts, setPosts] = useState([]) 
-    //1. change posts to post and set it to empty object instead of empty array
     const [post, setPost] = useState({})
     const [id, setId] = useState(1)
+    const [idFromButtonClick, setIdFromButtonClick] = useState(1)
 
     useEffect(() => {
-        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`) //2. change the url to append the post id
+        axios
+            .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
             .then(res => {
                 console.log(res)
-                setPost(res.data) //3. change setPosts to setPost
+                setPost(res.data)
             })
             .catch(err => {
                 console.log(err)
             })
-    }, [id]) //4. Add id to the dependency list
+    }, [idFromButtonClick])
+
+    const handleClick = () => {
+        setIdFromButtonClick(id)
+    }
     return ( 
         <div>
-            { /* 5. Add an input button */ }
             <input type="text" value={id} onChange={e => setId(e.target.value)}/>
+            <button onClick={handleClick}>Fetch Post</button>
             <div>{post.title}</div>
-            { /* 6. comment out the code for displating posts data */ }
-            {/* <ul>
-                { 
-                    posts.map(post => <li key={post.id}>{post.title}</li>)
-                }
-            </ul> */}
         </div>
      );
 }
