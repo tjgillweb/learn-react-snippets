4aef53d8594e48db8f0664f70796d45d8f94616d
tjgillweb <tjgillweb@gmail.com>
2020-06-24 17:35:39 +0530

    Add App.js and  DataFetching component

---
+++ fetching data useEffect/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import DataFetching from './components/DataFetching'
+
+function App() {
+  return (
+    <div className="App">
+      <DataFetching />
+    </div>
+  );
+}
+
+export default App;
