2148d992f2220d8b9e9251092eee5f5e618f1cb1
tjgillweb <tjgillweb@gmail.com>
2020-06-26 20:20:22 +0530

    Refs using createRef and Callback Ref

---
+++ refs/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import RefsDemo from './components/RefsDemo'
+
+function App() {
+  return (
+    <div className="App">
+      <RefsDemo />
+    </div>
+  );
+}
+
+export default App;
