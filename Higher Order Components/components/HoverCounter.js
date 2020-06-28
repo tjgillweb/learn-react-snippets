f369d13d3568a41552c31dffaef6df09d8abb3d3
tjgillweb <tjgillweb@gmail.com>
2020-06-28 22:38:43 +0530

    Add Higher Order Component and other components

---
+++ Higher Order Components/components/HoverCounter.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import withCounter from './withCounter';
+
+class HoverCounter extends Component {
+    
+    render() { 
+        const { count, incrementCount } = this.props
+        return ( 
+            <h2 onMouseOver={incrementCount}>{this.props.name} Hovered {count} times</h2>
+         );
+    }
+}
+ 
+export default withCounter(HoverCounter);
