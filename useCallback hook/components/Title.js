ae5b82bccf43a1e1614d0b03b973c80d0d9a8317
tjgillweb <tjgillweb@gmail.com>
2020-07-04 22:57:17 +0530

    use React.memo to avoid re-renders

---
+++ useCallback hook/components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Title = () => {
+    console.log('Rendering Title')
+    return ( 
+        <h2>
+            useCallback hook
+        </h2>
+     );
+}
+ 
+export default React.memo(Title);
