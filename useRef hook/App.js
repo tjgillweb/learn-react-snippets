ab55c756cb1bf826d803d47554eb68da473fe448
tjgillweb <tjgillweb@gmail.com>
2020-07-06 18:30:18 +0530

    add useref hook to hold a mutable value

--- useRef hook/App.js
+++ useRef hook/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import ClassTimer from './components/ClassTimer';
+import HookTimer from './components/HookTimer';
 
 function App() {
   return (
     <div className="App">
       <ClassTimer />
+      <HookTimer />
     </div>
   );
 }
