f194ebae6263690ccb7eda628fe457dd19f68f06
tjgillweb <tjgillweb@gmail.com>
2020-07-06 14:44:39 +0530

    Focus input field on page load with useRef hook

---
+++ useRef hook/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import FocusInput from './components/FocusInput';
+
+function App() {
+  return (
+    <div className="App">
+      <FocusInput />
+    </div>
+  );
+}
+
+export default App;
