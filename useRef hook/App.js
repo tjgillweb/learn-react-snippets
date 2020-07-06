facf507c8243c6be12216ec51f7bfdcc341e9cb2
tjgillweb <tjgillweb@gmail.com>
2020-07-06 15:17:33 +0530

    Add Class component interval timer

--- useRef hook/App.js
+++ useRef hook/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import FocusInput from './components/FocusInput';
+import ClassTimer from './components/ClassTimer';
 
 function App() {
   return (
     <div className="App">
-      <FocusInput />
+      <ClassTimer />
     </div>
   );
 }
