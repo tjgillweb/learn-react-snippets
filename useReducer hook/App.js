921ffdb102c331a894b13766ffa2675111b38e53
tjgillweb <tjgillweb@gmail.com>
2020-07-12 22:29:43 +0530

    useReducer(simple state and action)

---
+++ useReducer hook/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CounterOne from './components/CounterOne'
+
+function App() {
+  return (
+    <div className="App">
+      <CounterOne />
+    </div>
+  );
+}
+
+export default App;
